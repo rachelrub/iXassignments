e05f69e90d40b51b2ebb0d683ef0f95895244df7
rachelrub <rachel.rub14@gmail.com>
2016-06-09 10:35:20 +0200

    DAy 8 AftErNOOn git add . ! yay

--- Day8/form_script.js
+++ Day8/form_script.js
@@ -4,24 +4,51 @@ $(document).ready(function(){
         emailValidator($("#email").val());
 
 });
-
-$("#sumbit-btn").click(function(){ 
 });
 
-
-function newPhone(value) {
-    var phone = value.split("-");
-    var areaCode = parseInt([0], 10),
-        mid = parseInt([1], 10),
-        end = parseInt([2], 10);
-    return new Date(areaCode, mid, end);
+function phoneValidator(phoneNum) {
+	// I want to check that the form is XXX-XXX-XXXX
+	// split by "-"
+	// test amt of numbers per section
+	// if any section fails, then entire thing is wrong
+	// if all sections pass then YAY 
+    var phone = phoneNum.split("-");
+    console.log(phone);
+    if (phone.length !== 3) {
+    	$(".testPhone").text("Invalid Phone Number-- TRY AGAIN BITCH");
+    }
+    else if ((phone[0].length !== 3)||(phone[0] === NaN)) {
+    	$(".testPhone").text("Invalid Phone Number-- TRY AGAIN BITCH");
+    }
+    else if ((phone[1].length !== 3)||(phone[1] === NaN)) {
+    	$(".testPhone").text("Invalid Phone Number-- TRY AGAIN BITCH");
+    }
+    else if ((phone[2].length !== 4)||(phone[2] === NaN)) {
+    	$(".testPhone").text("Invalid Phone Number-- TRY AGAIN BITCH");
+    }
+    else {
+    	$(".testPhone").text("Woot Woot nice digets");
+    }
 }
+
+function emailValidator(email) {
+	// mmmmm@mmmmm.mmm
+	var e = email.split("@");
+	if (e.length !== 2) {
+		$(".testEmail").text("Invalid Email -- TRY AGAIN BITCH");
+	}
+	var com = e[1].split(".");
+	if (com.length !==2) {
+		$(".testEmail").text("Invalid Email -- TRY AGAIN BITCH");
+	}
+	else {
+		$(".testEmail").text("Congrats good email");
+	}
+}
+
+
+
+
 	
-$("#phone").validate( {
 
-});
-
-$("#email").validate({
-
-});
 
